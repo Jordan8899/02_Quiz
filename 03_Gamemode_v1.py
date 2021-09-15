acc1b195488b77580b83de4809eb66977e98292f
Jordan8899 <burmei.jorda@awatapu.school.nz>
2021-09-15 15:05:17 +1200

    Gamemode function

--- 03_Gamemode_v1.py
+++ 03_Gamemode_v1.py
@@ -1,4 +1,18 @@
 
 # List of game modes
+print("This quiz has multiple gamemode having: 1 'timed', 2 'difficult', 3 'easy'")
 
 # Select Game Mode
+gamemode_selection = input("What gamemode would you like to select? ")
+
+if gamemode_selection == "1" or "timed":
+    print("You have selected the timed gamemode")
+
+elif gamemode_selection == "2" or "difficult":
+    print("You have selected the difficult gamemode")
+
+elif gamemode_selection == "3" or "easy":
+    print("You have selected the easy gamemode")
+
+else:
+    print("You have not selected a gamemode")
---
+++ 03_Gamemode_v2.py
@@ -0,0 +1,23 @@
+i = True
+# List of game modes
+print("This quiz has multiple gamemode having: 'timed', 'difficult', 'easy'")
+
+# Select Game Mode
+
+while i:
+    gamemode_selection = input("What gamemode would you like to select? ")
+
+    if gamemode_selection == "timed":
+        print("You have selected the timed gamemode")
+        i = False
+
+    elif gamemode_selection == "difficult":
+        print("You have selected the difficult gamemode")
+        i = False
+
+    elif gamemode_selection == "easy":
+        print("You have selected the easy gamemode")
+        i = False
+
+    else:
+        print("Please select one of the following gamemodes: 'timed' 'difficult' 'easy'")
