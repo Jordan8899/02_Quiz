5528cf962d9530ea6c73a19e18a34c7715332662
Jordan8899 <burmei.jorda@awatapu.school.nz>
2021-09-27 12:00:01 +1300

    Randomizer implicated however I need to link up the answers to the questions and delete said random questions and answers from the list once printed

--- 05_Questions_v1.py
+++ 05_Questions_v1.py
@@ -1,22 +1,34 @@
-
+import random
 # Hard Questions
-hard_questions = [""]
+hard_questions = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 # Easy Questions
-easy_questions = [""]
+easy_questions = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 # Timed Questions
-timed_questions = [""]
+timed_questions = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 # Hard Answers
-hard_answers = [""]
+hard_answers = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 # Easy Answers
-easy_answers = [""]
+easy_answers = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 # Timed Answers
-timed_answers = [""]
+timed_answers = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 # Gamemode selection
+gamemode = input("Gamemode? ")
+gamemode = gamemode.strip().lower()
 
-# Print using list
+# Print Questions based on gamemode
+if gamemode == "hard":
+    print(random.choice(hard_questions))
+
+
+if gamemode == "easy":
+    print(random.choice(easy_questions))
+
+
+if gamemode == "timed":
+    print(random.choice(timed_questions))
