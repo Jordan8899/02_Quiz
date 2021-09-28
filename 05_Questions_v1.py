c105509084c565e92c56fc23ef6074a974ffe114
Jordan8899 <burmei.jorda@awatapu.school.nz>
2021-09-28 13:28:49 +1300

    Randomizer works however error and like yea

--- 05_Questions_v1.py
+++ 05_Questions_v1.py
@@ -37,15 +37,67 @@ while not gamemode:
 
 
 # Print Questions based on gamemode
-while mode == "hard":
-    random.choice(hard_questions)
+if mode == "hard":
+    question = random.choice(hard_questions)
+    if question == hard_questions[0]:
+        print(hard_answers[0])
+        print(hard_answers[0])
+        del hard_questions[0]
+        del hard_answers[0]
+        question = random.choice(hard_questions)
+    elif question == hard_questions[1]:
+        print(hard_answers[1])
+        print(hard_answers[1])
+        del hard_questions[1]
+        del hard_answers[1]
+        question = random.choice(hard_questions)
+    elif question == hard_questions[2]:
+        print(hard_answers[2])
+        print(hard_answers[2])
+        del hard_questions[2]
+        del hard_answers[2]
+        question = random.choice(hard_questions)
+    elif question == hard_questions[3]:
+        print(hard_answers[3])
+        print(hard_answers[3])
+        del hard_questions[3]
+        del hard_answers[3]
+        question = random.choice(hard_questions)
+    elif question == hard_questions[4]:
+        print(hard_answers[4])
+        print(hard_answers[4])
+        del hard_questions[4]
+        del hard_answers[4]
+        question = random.choice(hard_questions)
+    elif question == hard_questions[5]:
+        print(hard_answers[5])
+        print(hard_answers[5])
+        del hard_questions[5]
+        del hard_answers[5]
+        question = random.choice(hard_questions)
+    elif question == hard_questions[6]:
+        print(hard_answers[6])
+        print(hard_answers[6])
+        del hard_questions[6]
+        del hard_answers[6]
+        question = random.choice(hard_questions)
+    elif question == hard_questions[7]:
+        print(hard_answers[7])
+        print(hard_answers[7])
+        del hard_questions[7]
+        del hard_answers[7]
+        question = random.choice(hard_questions)
+    elif question == hard_questions[8]:
+        print(hard_answers[8])
+        print(hard_answers[8])
+        del hard_questions[8]
+        del hard_answers[8]
+        question = random.choice(hard_questions)
 
-    quit()
-
-while mode == "easy":
+elif mode == "easy":
     print(random.choice(easy_questions))
     quit()
 
-while mode == "timed":
+elif mode == "timed":
     print(random.choice(timed_questions))
     quit()
