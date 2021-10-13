e96bc0ef9700fb69cd2f8d65cc38a59e9538e556
Jordan8899 <burmei.jorda@awatapu.school.nz>
2021-10-14 11:36:46 +1300

    Questions v1 works however I need to make this code print one question at a time and only ask the next question after I have inputted something

--- 05_Questions_v1.py
+++ 05_Questions_v1.py
@@ -6,17 +6,11 @@ hard_questions = ["1", "2", "3", "4", "5", "6", "7", "8"]
 # Easy Questions
 easy_questions = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
-# Timed Questions
-timed_questions = ["1", "2", "3", "4", "5", "6", "7", "8"]
-
 # Hard Answers
 hard_answers = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
 # Easy Answers
 easy_answers = ["1", "2", "3", "4", "5", "6", "7", "8"]
-
-# Timed Answers
-timed_answers = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
 # Gamemode selection
 i = False
@@ -28,28 +22,28 @@ while not i:
         i = True
     elif gamemode == "easy":
         i = True
-    elif gamemode == "timed":
-        i = True
     else:
         print("Please input")
         i = False
 
+# Question Selection
+if gamemode == "hard":
+    questions = hard_questions
+    answers = hard_answers
 
-# Print Questions based on gamemode
-while gamemode == "hard":
-    question = random.choice(hard_questions)
-    if question == hard_questions[0]:
-        print(hard_answers[0])
-        print(hard_answers[0])
-        del hard_questions[0]
-        del hard_answers[0]
-        question = random.choice(hard_questions)
+elif gamemode == "easy":
+    questions = easy_questions
+    answers = easy_answers
 
 
-while gamemode == "easy":
-    print(random.choice(easy_questions))
-    quit()
+def randomizer():
+    questions_amount = len(questions) - 1
+    randomized_number = random.randint(0, questions_amount)
+    return[questions[randomized_number], answers[randomized_number]]
 
-while gamemode == "timed":
-    print(random.choice(timed_questions))
-    quit()
+while len(questions) > 0:
+    randomized = randomizer()
+    print(randomized[0])
+    print(randomized[1])
+    questions.remove(randomized[0])
+    answers.remove(randomized[1])
--- main.py
+++ main.py
@@ -1,15 +1,49 @@
 import random
 
-question = ["1", "2"]
-x = random.randint(0, 1)
-while question:
-    print(question[x])
+# Hard Questions
+hard_questions = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
-    if question[x] == question[0]:
-        print("Question 1")
-        del question[x]
-        x = random.randint(0, 1)
+# Easy Questions
+easy_questions = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
-    elif question[x] == question[1]:
-        print("Question 2")
-        x = random.randint(0, 1)
+# Hard Answers
+hard_answers = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
+# Easy Answers
+easy_answers = ["1", "2", "3", "4", "5", "6", "7", "8"]
+
+# Gamemode selection
+i = False
+while not i:
+    gamemode = input("Gamemode? ")
+    gamemode = gamemode.strip().lower()
+    if gamemode == "hard":
+        gamemode = "hard"
+        i = True
+    elif gamemode == "easy":
+        i = True
+    else:
+        print("Please input")
+        i = False
+
+# Question Selection
+if gamemode == "hard":
+    questions = hard_questions
+    answers = hard_answers
+
+elif gamemode == "easy":
+    questions = easy_questions
+    answers = easy_answers
+
+
+def randomizer():
+    questions_amount = len(questions) - 1
+    randomized_number = random.randint(0, questions_amount)
+    return[questions[randomized_number], answers[randomized_number]]
+
+while len(questions) > 0:
+    randomized = randomizer()
+    print(randomized[0])
+    print(randomized[1])
+    questions.remove(randomized[0])
+    answers.remove(randomized[1])
