2970a84804ee97af425aef65adb6e4bd08f9bed8
Jordan8899 <burmei.jorda@awatapu.school.nz>
2021-10-26 14:09:26 +1300

    This works completely, work next time on normal questions and then combine to main program and test

--- 05_Questions_v2.py
+++ 05_Questions_v2.py
@@ -4,44 +4,44 @@ score = 0
 round_number = 1
 
 # Hard Questions
-hard_questions = ["1\n",
-                  "2\n",
-                  "3\n",
-                  "4\n",
-                  "5\n",
-                  "6\n",
-                  "7\n",
-                  "8\n"]
+hard_questions = ["What is the motto of Hogwarts school for witchcraft and wizarding (in English)?\n",
+                  "What ghost is missing from the movies that appears in the books?\n",
+                  "What character does JK Rowling share the same birthday with?\n",
+                  "Where did JK Rowling get Heromione’s name from?\n",
+                  "What character mentioned in the first book / movie was taken from a real person?\n",
+                  "What language do spells come from?\n",
+                  "What secret character is in the films including fantastic beasts?\n",
+                  "What material were the brooms made out of in real life (3 words)?\n"]
 
 # Easy Questions
 easy_questions = ["What is the name of the spell that gave Harry Potter the scar on his forehead?\n",
-                  "2\n",
-                  "3\n",
-                  "4\n",
-                  "5\n",
-                  "6\n",
-                  "7\n",
-                  "8\n"]
+                  "What house did Harry Potter get sorted into?\n",
+                  "What is the REAL name of the Dark Lord?\n",
+                  "What teacher at Hogwarts was a werewolf (full name)?\n",
+                  "What is the name of the wizardring town outside of Hogwarts?\n",
+                  "What character was in love with Lily Potter (full name)?\n",
+                  "What subject did Albus Dumbledoor teach before becoming headmaster?\n",
+                  "What book number has the highest word count?\n"]
 
 # Hard Answers
-hard_answers = ["1",
-                "2",
-                "3",
-                "4",
-                "5",
-                "6",
-                "7",
-                "8"]
+hard_answers = ["Never tickle a sleeping dragon",
+                "Peeves",
+                "Harry Potter",
+                "Shakespeare",
+                "Nicholas Flamel",
+                "Latin",
+                "Ginger Witch",
+                "Airplane grade titanium"]
 
 # Easy Answers
-easy_answers = ["avadakedavra",
-                "2",
-                "3",
-                "4",
-                "5",
-                "6",
-                "7",
-                "8"]
+easy_answers = ["Avada Kedavra",
+                "Gryffindor",
+                "Tom Riddle",
+                "Remus Lupin",
+                "Hogsmeade",
+                "Severous Snape",
+                "Transfiguration",
+                "5"]
 
 # Gamemode selection
 i = False
@@ -61,16 +61,19 @@ while not i:
 if gamemode == "hard":
     questions = hard_questions
     answers = hard_answers
+    printed_answer = hard_answers
 
 elif gamemode == "easy":
     questions = easy_questions
     answers = easy_answers
+    printed_answer = easy_answers
+
 
 # Randomized question function
 def randomizer():
     questions_amount = len(questions) - 1
     randomized_number = random.randint(0, questions_amount)
-    return[questions[randomized_number], answers[randomized_number]]
+    return[questions[randomized_number], answers[randomized_number], printed_answer[randomized_number]]
 
 # Prints said randomized question which is linked to the answer while also adding to the score if correct and adding round number
 while len(questions) > 0:
@@ -82,7 +85,7 @@ while len(questions) > 0:
         print("Correct\n")
         score += 1
     elif guess != randomized[1]:
-        print("Incorrect, the correct answer is {}\n".format(randomized[0]))
+        print("Incorrect, the correct answer is {}\n".format((randomized[2])))
     round_number += 1
     questions.remove(randomized[0])
     answers.remove(randomized[1])
